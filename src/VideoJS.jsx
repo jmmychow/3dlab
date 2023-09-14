0e737cdf3f070d40b140bc22350c5a2f2461bb7b
cgriders <cgriders>
2023-09-14 08:13:29 +0800

    fix video-js.css not found

--- src/VideoJS.jsx
+++ src/VideoJS.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import videojs from 'video.js';
+import 'videojs-vr';
+import 'video.js/dist/video-js.css';
+import 'videojs-vr/dist/videojs-vr.css';
 
 export const VideoJS = (props) => {
   const videoRef = React.useRef(null);
