d299384a458f54a83b60ecea4e546b4fbf6bc78a
cgriders <cgriders>
2023-09-21 01:36:39 +0800

    commit

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -4,23 +4,19 @@ export const LoadingScreen = ({ started, onStarted }) => {
   const { progress } = useProgress();
   return (
     <div className={`loadingScreen ${started ? "loadingScreen--started" : ""}`}>
-      {/*<div className="loadingScreen__progress">
-        <div
-          className="loadingScreen__progress__value"
-          style={{
-            width: `${progress}%`,
-          }}
-        />
-        </div>*/}
       <div className="loadingScreen__board">
         <button
-          className="loadingScreen__button"
-          disabled={progress < 100}
-          onClick={onStarted}
-        >
-          Click to start
-        </button><br/><br/>
-        {Math.floor(progress)}% loaded
+            className="loadingScreen__button bg-opacity-0"
+            disabled={progress < 100}
+            onClick={onStarted}
+          >
+          {(progress==100 ? "Click to start" : Math.floor(progress).toString()+"% loaded" )}
+        </button>
+        <div className="stars"></div>
+        <div className="stars"></div>
+        <div className="stars"></div>
+        <div className="stars"></div>
+        <div className="stars"></div>
       </div>
     </div>
   );
